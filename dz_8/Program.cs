f524141a38d96267c266a1bf2151daac2591aada
Владислав Андреевич Минаков <s0160144@edu.kubsu.ru>
2021-12-24 20:00:51 +0300

    8

---
+++ dz_8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace dz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string graph1 = null;
+            string graph2 = null;
+
+            graph1 = File.ReadAllText(@"C:\Users\Владислав\source\repos\dz\dz\dz_8\1.txt");
+            graph2 = File.ReadAllText(@"C:\Users\Владислав\source\repos\dz\dz\dz_8\2.txt");
+
+            if (graph1 == graph2)
+                Console.WriteLine("Подстановка является автоморфизмом");
+            else Console.WriteLine("Подстановка не является автоморфизмом");
+            Console.ReadKey();
+        }
+    }
+}
